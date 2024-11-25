3a140580719006293781887115dba919e6df1148
Thomas <t.missak595@gmail.com>
2024-11-25 10:43:43 +0100

    [add] Model interfaces for Olympic countries and Participations

--- src/app/core/models/Participation.ts
+++ src/app/core/models/Participation.ts
@@ -1,11 +1,6 @@
-// TODO: create here a typescript interface for a participation
-/*
-example of participation:
-{
-    id: 1,
-    year: 2012,
-    city: "Londres",
-    medalsCount: 28,
-    athleteCount: 372
+export interface Participation {
+    id: number;
+    year: number; // Année des Jeux Olympiques
+    medalsCount: number;
+    athleteCount: number;
 }
-*/
